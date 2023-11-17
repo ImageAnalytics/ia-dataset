33a4287801377b6d43c39267cdb93ee0cb2f5f2c
Ross Marchant <ross.g.marchant@gmail.com>
2023-11-17 23:22:53 +1100

    Instance segmentation

--- ia_dataset/merge.py
+++ ia_dataset/merge.py
@@ -60,95 +60,28 @@ def merge(dataset: MLDataset,
 
     dataset.update_labels_from_annotations()
 
-    # tet
 
-    #                              new_project,
-    #                              add_missing_images=True,
-    #                              add_missing_annotations=True,
-    #                              overwrite_annotation_dimensions=False,
-    #                              overwrite_annotation_label=False,
-    #                              iou_threshold=0.5,
-    #                              add_all_annotations=False):
-    # print("Adding data from {}".format(new_project.filename))
-    # image_metadata_to_add = []
-    # count = 0
-    # overlap_count = 0
-    # new_image_count = 0
-    # annotation_count = 0
-    # new_annotation_count = 0
-    #
-    # ann_dict = dict()
-    # overlap_dict = dict()
-    # new_dict = dict()
-    #
-    # missing_key_count = 0
-    #
-    # # TODO when combining, need to ensure unique sequence ids
-    #
-    # # Loop through images in new project
-    # for key, new_metadata in new_project.image_dict.items():
-    #     # If the image is already in the old project ...
-    #     if key in self.image_dict:
-    #         metadata = self.image_dict[key]
-    #         # Check at each the annotation in the new project image metadata ...
-    #         for new_annotation in new_metadata.annotations:
-    #             annotation_count += 1
-    #             if new_annotation.label in ann_dict:
-    #                 ann_dict[new_annotation.label] += 1
-    #             else:
-    #                 ann_dict[new_annotation.label] = 1
-    #             overlap = False
-    #             # ... against each in the current project,
-    #             for annotation in metadata.annotations:
-    #                 # and if there is any overlap then flag it
-    #                 # print(annotation.iou(new_annotation))
-    #                 if annotation.iou(new_annotation) > iou_threshold:
-    #                     overlap = True
-    #                     overlap_count += 1
-    #                     if overwrite_annotation_dimensions:
-    #                         annotation.x = new_annotation.x
-    #                         annotation.y = new_annotation.y
-    #                         annotation.width = new_annotation.width
-    #                         annotation.height = new_annotation.height
-    #                     if overwrite_annotation_label:
-    #                         annotation.label = new_annotation.label
-    #                         annotation.score = new_annotation.score
-    #                         annotation.annotator = new_annotation.annotator
-    #                     if new_annotation.label in overlap_dict:
-    #                         overlap_dict[new_annotation.label] += 1
-    #                     else:
-    #                         overlap_dict[new_annotation.label] = 1
-    #                     # break
-    #             # but if no overlap, then add the annotation from the new_project
-    #             if not add_all_annotations:
-    #                 # print(new_annotation.label)
-    #                 if overlap is False and add_missing_annotations is True:
-    #                     metadata.add_annotation(new_annotation)
-    #                     new_annotation_count += 1
-    #                     if new_annotation.label in new_dict:
-    #                         new_dict[new_annotation.label] += 1
-    #                     else:
-    #                         new_dict[new_annotation.label] = 1
-    #             else:
-    #                 metadata.add_annotation(new_annotation)
-    #                 new_annotation_count += 1
-    #                 if new_annotation.label in new_dict:
-    #                     new_dict[new_annotation.label] += 1
-    #                 else:
-    #                     new_dict[new_annotation.label] = 1
-    #     # Image not found - add annotation
-    #     elif add_missing_images:
-    #         new_image_count += 1
-    #         image_metadata_to_add.append(new_metadata)
-    #         annotation_count += len(new_metadata.annotations)
-    #         new_annotation_count += len(new_metadata.annotations)
-    #     else:
-    #         missing_key_count += 1
-    # print("Results")
-    # print(" - annotations processed: {}, {}".format(annotation_count, ann_dict))
-    # print(" --- overlapping: {}, {}".format(overlap_count, overlap_dict))
-    # print(" --- added: {}, {}".format(new_annotation_count - overlap_count, new_dict))
-    # print(" - images added: {}".format(new_image_count))
-    # print(" - # keys that could not be matched: {}".format(missing_key_count))
-    # for metadata in image_metadata_to_add:
-    #     self.add_image(metadata)
+def remove_overlapping_image(image: MLImage, iou_threshold: float = 0.5):
+    annotations = sorted(image.annotations, key=lambda x: x.score, reverse=True)
+
+    # Initialize a list to keep track of non-overlapping annotations
+    non_overlapping_annotations = []
+
+    while annotations:
+        # Take the annotation with the highest score
+        current_annotation = annotations.pop(0)
+        non_overlapping_annotations.append(current_annotation)
+
+        # Remove annotations that overlap with the current one beyond the IoU threshold
+        annotations = [
+            annotation for annotation in annotations
+            if current_annotation.iou(annotation) <= iou_threshold
+        ]
+
+    # Replace the original list of annotations with non-overlapping ones
+    image.annotations = non_overlapping_annotations
+
+
+def remove_overlapping(dataset: MLDataset, iou_threshold: float = 0.5):
+    for image in dataset.images:
+        remove_overlapping_image(image, iou_threshold)
